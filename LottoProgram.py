75760aaa64d82a971d31e8d34ea9a6d86f5f70f3
codeleesh <38518639+codeleesh@users.noreply.github.com>
2020-05-04 11:13:53 +0900

    total random number choice & select sort

--- LottoProgram.py
+++ LottoProgram.py
@@ -2,6 +2,7 @@ import random, string
 
 cn = "y"
 
+print("보람아 사랑해!")
 print("로또 번호 자동 생성 프로그램 시작")
 print("-------------------------------------------")
 print("게임수를 입력하세요. 숫자만 입력 가능합니다. :")
@@ -13,16 +14,29 @@ while( cn == "Y" or cn == "y" ):
     if( num.isdigit() == True ) :
 
         print("-------------------------------------------")
-
+        print("보람아 사랑해!")
+        print("랜덤으로 뽑은 Lotto 숫자")
         for i in range(0, int(num)):
             lotto = random.sample(range(1,46), 6)
+            if( i == 0) :
+                choiceLotto = lotto[:]
+            else : 
+                choiceLotto.extend(lotto)
             lotto.sort()
             print(lotto)
+
+        choiceLotto = list(set(choiceLotto))
+        print("랜덤으로 뽑힌 Lotto 숫자 중에서 다시 랜덤으로 뽑은 숫자")
+        for i in range(0, int(num)):
+            newLotto = random.sample(choiceLotto, 5)
+            newLotto.sort()
+            print(newLotto)
     else:
         print("숫자를 입력하세요.")
         continue
 
     print("-------------------------------------------")
+    print("보람아 사랑해!")
     print("로또 번호 자동 생성 완료")
     print("-------------------------------------------")
     cn = input("다시 하시겠습니까(Y/N)? :")
@@ -35,4 +49,5 @@ while( cn == "Y" or cn == "y" ):
 print("-------------------------------------------")
 print("로또 번호 자동 생성 프로그램 종료")
 print("Enter 를 누르면 프로그램이 꺼집니다.")
+print("보람아 사랑해!")
 input(":")
