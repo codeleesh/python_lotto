94f8517f8b07aed0be555c7f8c552d0f4a4f9d31
codeleesh <38518639+codeleesh@users.noreply.github.com>
2020-11-03 13:04:12 +0900

    파이썬 일부 로직 개선 및 변경

--- LottoProgram.py
+++ LottoProgram.py
@@ -2,12 +2,15 @@ import random, string
 
 cn = "y"
 
+print("BR♡PD♥SH")
 print("로또 번호 자동 생성 프로그램 시작")
 print("-------------------------------------------")
 print("게임수를 입력하세요. 숫자만 입력 가능합니다. :")
 
 while( cn == "Y" or cn == "y" ):
-
+    print("-------------------------------------------")
+    print("변수 초기화")
+    lotto = None
     num = input("숫자입력 :")
 
     if( num.isdigit() == True ) :
@@ -16,19 +19,16 @@ while( cn == "Y" or cn == "y" ):
         print("랜덤으로 뽑은 Lotto 숫자")
         for i in range(0, int(num)):
             lotto = random.sample(range(1,46), 6)
-            if( i == 0) :
-                choiceLotto = lotto[:]
-            else : 
-                choiceLotto.extend(lotto)
             lotto.sort()
             print(lotto)
+            lotto = None
 
-        choiceLotto = list(set(choiceLotto))
-        print("랜덤으로 뽑힌 Lotto 숫자 중에서 다시 랜덤으로 뽑은 숫자")
-        for i in range(0, int(num)):
-            newLotto = random.sample(choiceLotto, 6)
-            newLotto.sort()
-            print(newLotto)
+        # choiceLotto = list(set(choiceLotto))
+        # print("랜덤으로 뽑힌 Lotto 숫자 중에서 다시 랜덤으로 뽑은 숫자")
+        # for i in range(0, int(num)):
+        #     newLotto = random.sample(choiceLotto, 6)
+        #     newLotto.sort()
+        #     print(newLotto)
     else:
         print("숫자를 입력하세요.")
         continue
@@ -46,4 +46,5 @@ while( cn == "Y" or cn == "y" ):
 print("-------------------------------------------")
 print("로또 번호 자동 생성 프로그램 종료")
 print("Enter 를 누르면 프로그램이 꺼집니다.")
+print("BR♡PD♥SH")
 input(":")
